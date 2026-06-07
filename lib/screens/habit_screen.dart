6836e1e7534d401314ab1ce29dc0a31422b695b1
abdulrehman202 <abdulrehman1411@gmail.com>
2026-06-08 01:48:12 +0500

    .

--- lib/screens/habit_screen.dart
+++ lib/screens/habit_screen.dart
@@ -27,36 +27,41 @@ class _AllHabitsState extends ConsumerState<AllHabits> {
     _selectedCategory = ref.watch(habitsCategoryProvider);
     List<Habit> habits = ref.watch(categorizedlist);
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ChipsChoice<Category>.single(
-          choiceStyle: C2ChipStyle.filled(
-        color: Colors.white,
-        selectedStyle:  C2ChipStyle(
-          foregroundColor: Colors.white,
-          backgroundColor:  Theme.of(context).colorScheme.primary,
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
+    return Scaffold(
+      appBar: AppBar(title: Text('All Habits'), automaticallyImplyLeading: false,),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ChipsChoice<Category>.single(
+            padding: EdgeInsets.zero,
+            choiceStyle: C2ChipStyle.filled(
+          color: Colors.white,
+          selectedStyle:  C2ChipStyle(
+            foregroundColor: Colors.white,
+            backgroundColor:  Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
           ),
         ),
+            choiceItems: Category.values.map((element)=>C2Choice(value: element, label: element.name.capitalize)).toList(),
+            value: _selectedCategory, onChanged: (category)=>ref.read(habitsCategoryProvider.notifier).changeCategory(category)),
+      
+             Expanded(
+               child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                 child: ListView.separated(
+                  physics: ScrollPhysics(),
+                  separatorBuilder: (context, index) => Divider(),
+                  shrinkWrap: true,
+                  itemCount: habits.length,
+                  itemBuilder: (context, index)=> HabitTile(habit: habits[index])),
+               ),
+             )
+          
+        ],
       ),
-          choiceItems: Category.values.map((element)=>C2Choice(value: element, label: element.name.capitalize)).toList(),
-          value: _selectedCategory, onChanged: (category)=>ref.read(habitsCategoryProvider.notifier).changeCategory(category)),
-
-           Expanded(
-             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10),
-               child: ListView.separated(
-                physics: ScrollPhysics(),
-                separatorBuilder: (context, index) => Divider(),
-                shrinkWrap: true,
-                itemCount: habits.length,
-                itemBuilder: (context, index)=> HabitTile(habit: habits[index])),
-             ),
-           )
-        
-      ],
     );
   }
 }
